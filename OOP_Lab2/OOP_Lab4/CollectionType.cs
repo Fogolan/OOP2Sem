f96150f4126b565ab8ddbd626b5408a770e9859f
Fogolan <Fogolan@gmail.com>
2017-03-19 22:08:39 +0300

    add fourth lab

---
+++ OOP_Lab2/OOP_Lab4/CollectionType.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace OOPLab2_04
+{
+    class CollectionType<T> : IEnumerable<T> where T : Pentagon
+    {
+        private LinkedList<T> collectionList;
+        private LinkedListNode<T> listNode;
+        private static readonly Random Rnd;
+        public static int objCount;
+
+        static CollectionType()
+        {
+            Rnd = new Random();
+            objCount = 0;
+        }
+        public CollectionType()
+        {
+            collectionList = new LinkedList<T>();
+            objCount++;
+            CollectionNum = objCount;
+        }
+        public CollectionType(int count)
+        {
+            collectionList = new LinkedList<T>();
+            objCount++;
+            CollectionNum = objCount;
+            for (int i = 0; i < count; i++)
+                collectionList.AddLast((T) new Pentagon(Rnd.Next(1, 100)));
+        }
+        ~CollectionType()
+        {
+            objCount--;
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                if (i < 0 || i > collectionList.Count)
+                    throw new IndexOutOfRangeException();
+                listNode = collectionList.First;
+                for (int ii = 0; ii < i; ii++)
+                {
+                    listNode = listNode.Next;
+                }
+                return listNode.Value;
+            }
+            set
+            {
+                if (i < 0 || i > collectionList.Count)
+                    throw new IndexOutOfRangeException();
+                for (int ii = 0; ii < i; ii++)
+                {
+                    listNode = listNode.Next;
+                }
+                listNode.Value = value;
+            }
+        }
+        public LinkedList<T> CollectionList { get { return collectionList; } set { collectionList = value; } }
+        public int CollectionNum { get; }
+        public int ObjCount => objCount;
+
+        public void AddLast(T obj)
+        {
+            collectionList.AddLast(obj);
+        }
+        public T Remove(T obj)
+        {
+            var element = collectionList.FirstOrDefault(i => Equals(i, obj));
+            if (element != null)
+            {
+                collectionList.Remove(element);
+                return element;
+            }
+            throw new NullReferenceException();
+        }
+        public void WriteToFile()
+        {
+            using (StreamWriter sw = new StreamWriter($"out{CollectionNum}.txt"))
+            {
+                sw.WriteLine(ToString());
+            }
+        }
+        public void Sort()
+        {
+            collectionList = new LinkedList<T>(collectionList.OrderBy(i => i.Side));
+            Console.WriteLine("Коллекция после сортировки ");
+            Console.WriteLine(ToString());
+        }
+
+        public override string ToString()
+        {
+            string res = $"\nКоллекция №{CollectionNum}\n";
+            for (int i = 0; i < collectionList.Count; i++)
+                res += $"{i}. {this[i]}\n";
+            return res;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return collectionList.GetEnumerator();
+        }
+    }
+}
---
+++ OOP_Lab2/OOP_Lab4/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace OOPLab2_04
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            CollectionType<Pentagon> collection1 = new CollectionType<Pentagon>(10);
+            CollectionType<Pentagon> collection2 = new CollectionType<Pentagon>(10);
+            CollectionType<Pentagon> collection3 = new CollectionType<Pentagon>(12);
+            CollectionType<Pentagon> collection4 = new CollectionType<Pentagon>(15);
+            CollectionType<Pentagon> collection5 = new CollectionType<Pentagon>(5);
+            List<CollectionType<Pentagon>> list = new List<CollectionType<Pentagon>>
+            {
+                collection1,
+                collection2,
+                collection3,
+                collection4,
+                collection5
+            };
+            Console.WriteLine(collection1.ToString());
+            Console.WriteLine(collection2.ToString());
+            collection2.WriteToFile();
+            collection2.Sort();
+
+            int collectionCount = 0;
+            foreach (var collection in list)
+            {
+                if (collection.Count() == 10)
+                    collectionCount++;
+            }
+            Console.WriteLine($"Коллекция размером 10 = {collectionCount}");
+
+            int minCollection = Int32.MaxValue;
+            CollectionType<Pentagon> minCollectionIndex = new CollectionType<Pentagon>();
+            int maxCollection = Int32.MinValue;
+            CollectionType<Pentagon> maxCollectionIndex = new CollectionType<Pentagon>();
+            foreach (var collection in list)
+            {
+                if (collection.Min(i => i.Height) < minCollection)
+                {
+                    minCollection = collection.Min(i => i.Height);
+                    minCollectionIndex = collection;
+                }
+
+                if (collection.Max(i => i.Height) > maxCollection)
+                {
+                    maxCollection = collection.Max(i => i.Height);
+                    maxCollectionIndex = collection;
+                }
+                    
+
+            }
+            Console.WriteLine($"Минимальная коллекция {minCollectionIndex}\n" +
+                              $"Максимальная коллекция {maxCollectionIndex}");
+
+
+            var linq1 = collection1.Where(i => i.Side < 20).Select(i => i.ToString()).OrderBy(i => i.Length).ToList();
+            foreach (var obj in linq1)
+                Console.WriteLine(obj);
+
+            var linq2 = list.First().OrderByDescending(i=>i.Side).Any(i => i.Side > 50);
+            Console.WriteLine(linq2);
+
+            var linq3 = collection2.Select(i => i.Height).Where(i => i > 1000).Sum();
+            Console.WriteLine(linq3);
+
+            var linq4 = collection4.Union(collection5.Where(i => i.Side > 70)).All(i => i != null);
+            Console.WriteLine(linq4);
+
+            var linq5 = list.Last().Select(i => i.Height).ToList().SkipWhile(i => i < 100).ToList();
+            foreach (var obj in linq5)
+                Console.WriteLine(obj.ToString());
+
+
+
+            List<string> stringList = new List<string>
+            {
+                "съешь",
+                "этих",
+                "мягких",
+                "французских",
+                "булок",
+                "и",
+                "выпей",
+                "чаю",
+                "Ввод"
+            };
+
+            foreach (var obj in stringList)
+            {
+                Console.Write($"{obj}, ");
+            }
+            Console.WriteLine();
+
+            
+            using (StreamWriter sw = new StreamWriter("string.txt"))
+            {
+                foreach (var obj in stringList)
+                {
+                    sw.Write($"{obj}, ");
+                }
+            }
+
+            var findString = stringList.Find(i => i == "Ввод");
+            Console.WriteLine(findString);
+
+            int stringCount = stringList.Count(i => i.Length == 4);
+            Console.WriteLine(stringCount);
+
+            var asc = stringList.OrderBy(i => i.Length);
+            foreach (var str in asc)
+                Console.Write($"{str}, ");
+
+            Console.WriteLine();
+
+            var desc = stringList.OrderByDescending(i => i.Length);
+            foreach (var str in desc)
+                Console.Write($"{str}, ");
+
+            Console.WriteLine();
+        }
+    }
+}
